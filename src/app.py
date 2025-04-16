742196d10c66491c30e0968b67cadc806f846017
li-calvin <licalvin5858@gmail.com>
2025-04-15 20:28:40 -0400

    app change

--- src/app.py
+++ src/app.py
@@ -56,7 +56,7 @@ def get_random_item(env, category):
     with connection:
         with connection.cursor() as cursor:
             query = f"""
-                SELECT key, brand, color, price 
+                SELECT file, brand, color, price 
                 FROM uploads
                 WHERE category = %s 
                 ORDER BY RAND() LIMIT 1;
